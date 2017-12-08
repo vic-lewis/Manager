0377c94702b2c9510b7da73c28a3cc93580cc921
lewis <lewis@zhezhe.com.tw>
2017-12-08 15:39:05 +0800

    Initial commit

---
+++ manager_library/src/main/java/vicdna/com/manager_library/ToastUtils.java
@@ -0,0 +1,25 @@
+package vicdna.com.manager_library;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by lewis on 2017/12/8.
+ */
+
+public class ToastUtils {
+
+    /**
+     * 顯示多士
+     *
+     * @param context
+     * @param str
+     */
+    public static void showLongToast(Context context, String str) {
+        Toast.makeText(context, str, Toast.LENGTH_LONG).show();
+    }
+
+    public static void showShortToast(Context context, String str) {
+        Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
+    }
+}
